4bcf9540613489839fd17571da2d30fca53c9d82
Anderberg Graham <829546@stma.k12.mn.us>
2021-04-01 10:05:18 -0500

    Comparison

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if int(x > y):
+        return True
+    else:
+        return False
+
+def lessThan(x, y):
+    if int(x < y):
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if int(x == y):
+        return True
+    else:
+        return False
+
+def greaterOrEqual(x, y):
+    if int(x >= y):
+        return True
+    else:
+        return False
+    
+def lessOrEqual(x, y):
+    if int(x <= y):
+        return True
+    else:
+        return False
+
+print(greaterThan(120, 119))
+print(lessThan(119, 120))
+print(equalTo(120, 120))
+print(greaterOrEqual(121, 120))
+print(lessOrEqual(120, 121))
